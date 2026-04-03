2d3f58578e8d89408e42f725361b2a65f7785409
jerome natividad <Jeeromee7@gmail.com>
2026-04-03 15:05:23 +0800

    Configure Django to use Google SMTP backend for email authentication

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -186,6 +186,7 @@ CORS_EXPOSE_HEADERS = [
     "Authorization",
 ]
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 465
 EMAIL_USE_TLS = False
